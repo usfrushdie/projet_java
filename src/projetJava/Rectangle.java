77972b66930ac925fca9165ca3f3d1110119349e
Usf <you.rochdi02@gmail.com>
2024-03-16 15:43:13 +0100

    first commit

---
+++ src/projetJava/Rectangle.java
@@ -0,0 +1,62 @@
+package projetJava;
+
+public class Rectangle {
+	
+	private int x1, x2, y1, y2;
+
+	public Rectangle(int x1, int y1,int x2, int y2) {
+		this.x1 = x1;
+		this.x2 = x2;
+		this.y1 = y1;
+		this.y2 = y2;
+	}
+
+	public int getX1() {
+		return x1;
+	}
+
+	public void setX1(int x1) {
+		this.x1 = x1;
+	}
+
+	public int getX2() {
+		return x2;
+	}
+
+	public void setX2(int x2) {
+		this.x2 = x2;
+	}
+
+	public int getY1() {
+		return y1;
+	}
+
+	public void setY1(int y1) {
+		this.y1 = y1;
+	}
+
+	public int getY2() {
+		return y2;
+	}
+
+	public void setY2(int y2) {
+		this.y2 = y2;
+	}
+	
+	public void deplacerRect(int dx, int dy) {
+		this.x1 += dx;
+		this.x2 += dx;
+		this.y1 += dy;
+		this.y2 += dy;
+	}
+	
+	public boolean appartientPoint(int x, int y) {
+		return (x>=x1)&&(x<=x2)&&(y>=y1)&&(y<=y2);
+	}
+	
+	public static void main(String[] args) {
+		Rectangle r = new Rectangle(1,1,3,3);
+		System.out.println(r.appartientPoint(2,3));
+	}
+
+}
