95937f54511253263f308fc78b27559493a4da6a
Usf <you.rochdi02@gmail.com>
2024-03-16 17:04:09 +0100

    Ajout de la forme Cercle

---
+++ src/projetJava/Cercle.java
@@ -0,0 +1,61 @@
+package projetJava;
+
+public class Cercle {
+	
+	private double Xc;
+	private double Yc;
+	private double rayon;
+	
+	
+	public Cercle(double xc, double yc, double rayon) {
+		super();
+		Xc = xc;
+		Yc = yc;
+		this.rayon = rayon;
+	}
+
+	public double getXc() {
+		return Xc;
+	}
+
+	public void setXc(double xc) {
+		Xc = xc;
+	}
+
+	public double getYc() {
+		return Yc;
+	}
+
+	public void setYc(double yc) {
+		Yc = yc;
+	}
+
+	public double getRayon() {
+		return rayon;
+	}
+
+	public void setRayon(double rayon) {
+		this.rayon = rayon;
+	}
+
+	public void deplacerRect(int dx, int dy) {
+		this.Xc += dx;
+		this.Yc += dy;
+	}
+	
+	public boolean appartientPoint(int x, int y) {
+		return (Math.pow(Xc-x,2)+Math.pow(Yc-y,2))<=this.rayon*this.rayon;
+	}
+	
+	public void redimensionner(double k) {
+		this.rayon *= k;
+	}
+
+	public static void main(String[] args) {
+		Cercle c = new Cercle(0,0,3);
+		System.out.println(c.appartientPoint(2,3));
+		c.redimensionner(2.5);
+		System.out.println(c.appartientPoint(2,3));
+	}
+
+}
--- src/projetJava/Rectangle.java
+++ src/projetJava/Rectangle.java
@@ -1,61 +1,68 @@
 package projetJava;
 
-public class Rectangle {
+public class Rectangle implements Forme {
 	
-	private int x1, x2, y1, y2;
+	private double x1, x2, y1, y2;
 
-	public Rectangle(int x1, int y1,int x2, int y2) {
+	public Rectangle(double x1, double y1,double x2, double y2) {
 		this.x1 = x1;
 		this.x2 = x2;
 		this.y1 = y1;
 		this.y2 = y2;
 	}
 
-	public int getX1() {
+	public double getX1() {
 		return x1;
 	}
 
-	public void setX1(int x1) {
+	public void setX1(double x1) {
 		this.x1 = x1;
 	}
 
-	public int getX2() {
+	public double getX2() {
 		return x2;
 	}
 
-	public void setX2(int x2) {
+	public void setX2(double x2) {
 		this.x2 = x2;
 	}
 
-	public int getY1() {
+	public double getY1() {
 		return y1;
 	}
 
-	public void setY1(int y1) {
+	public void setY1(double y1) {
 		this.y1 = y1;
 	}
 
-	public int getY2() {
+	public double getY2() {
 		return y2;
 	}
 
-	public void setY2(int y2) {
+	public void setY2(double y2) {
 		this.y2 = y2;
 	}
 	
-	public void deplacerRect(int dx, int dy) {
+	public void deplacerRect(double dx, double dy) {
 		this.x1 += dx;
 		this.x2 += dx;
 		this.y1 += dy;
 		this.y2 += dy;
 	}
 	
-	public boolean appartientPoint(int x, int y) {
+	public boolean appartientPoint(double x, double y) {
 		return (x>=x1)&&(x<=x2)&&(y>=y1)&&(y<=y2);
+	}
+	
+	public void redimensionner(double k) {
+		this.x2 *= k;
+		this.y2 *= k;
 	}
 	
 	public static void main(String[] args) {
 		Rectangle r = new Rectangle(1,1,3,3);
+		System.out.println(r.appartientPoint(2,3));
+		r.redimensionner(0.5);
 		System.out.println(r.appartientPoint(2,3));
 	}
 
