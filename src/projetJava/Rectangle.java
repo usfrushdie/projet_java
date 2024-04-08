eb1863a5a634ffc587a0a9397435f491c0162f25
Hamza BAYA <bayahamza1010@gmail.com>
2024-04-08 02:06:25 +0200

    Update Rectangle.java

--- src/projetJava/Rectangle.java
+++ src/projetJava/Rectangle.java
@@ -8,7 +8,6 @@ import java.io.Serializable;
 public class Rectangle implements Forme{
 		
 	private int x1, x2, y1, y2;
-	private int width, height;
 
 	public Rectangle(int x1, int x2, int y1, int y2) {
 		this.x1 = x1;
